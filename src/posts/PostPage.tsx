0438cf2677283b33488490864475e757ee602372
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-10-07 12:02:22 +0300

    create-blog-post-page-component

---
+++ src/posts/PostPage.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { getPosts } from "./getPosts";
+import { PostData } from "./types";
+import { PostsList } from "./PostList";
+
+export function PostPage() {
+  const [isloading, setIsLoading] = useState(true);
+  const [posts, setPosts] = useState<PostData[]>([]);
+
+  useEffect(() => {
+    let cancel = false;
+    getPosts().then((data) => {
+      if (!cancel) {
+        setPosts(data);
+        setIsLoading(false);
+      }
+    });
+    return () => {
+      cancel = true;
+    };
+  }, []);
+  if (isloading) {
+    return <div className="w-96 mx-auto mt-6">Loading...</div>;
+  }
+  return (
+    <div className="w-96 mx-auto mt-6">
+      <h2 className="text-xl text-slate-900 font-bold">Posts</h2>
+      <PostsList posts={posts} />
+    </div>
+  );
+}
