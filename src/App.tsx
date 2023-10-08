68958b0138c4d02eadc5151c4cf516e2ea969e66
Rustem Mubarakshin <mubarakshin1996@gmail.com>
2023-10-08 15:12:33 +0300

    getting-data-using-react-query

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider, defer } from "react-router-dom";
 import { getPosts } from "./posts/getPosts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+
 const queryClient = new QueryClient();
 const router = createBrowserRouter([
   {
--- src/posts/PostPage.tsx
+++ src/posts/PostPage.tsx
@@ -1,11 +1,11 @@
-import { getPosts } from "./getPosts";
 import { PostData, NewPostData } from "./types";
 import { PostsList } from "./PostsList";
 import { savePost } from "./savePost";
 import { NewPostForm } from "./NewPostForm";
-import { assertIsPosts } from "./getPosts";
+import { assertIsPosts, getPosts } from "./getPosts";
 import { useLoaderData, Await } from "react-router-dom";
 import { Suspense } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 type Data = {
   posts: PostData[];
@@ -23,25 +23,36 @@ export function assertIsData(data: unknown): asserts data is Data {
 }
 
 export function PostsPage() {
-  const data = useLoaderData();
-  assertIsData(data);
+  const {
+    isLoading,
+    isFetching,
+    data: posts,
+  } = useQuery(["postsData"], getPosts);
+  // const data = useLoaderData();
+  // assertIsData(data);
+
   async function handleSave(newPostData: NewPostData) {
     await savePost(newPostData);
+  }
+
+  if (isLoading || posts === undefined) {
+    return <div className="w-96 mx-auto mt-6">Loading....</div>;
   }
 
   return (
     <div className="w-96 mx-auto mt-6">
       <h2 className="text-xl text-slate-900 font-bold">Posts</h2>
       <NewPostForm onSave={handleSave} />
+      {isFetching ? <div>Fetching...</div> : <PostsList posts={posts} />}
       {/* <PostsList posts={posts} /> */}
-      <Suspense fallback={<div>Fetching...</div>}>
+      {/*      <Suspense fallback={<div>Fetching...</div>}>
         <Await resolve={data.posts}>
           {(posts) => {
             assertIsPosts(posts);
             return <PostsList posts={posts} />;
           }}
         </Await>
-      </Suspense>
+      </Suspense> */}
     </div>
   );
 }
